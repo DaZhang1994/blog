34d2fbb4c8063fe437f5a079f6deabbb4607049d
DaZhang1994 <dazhang1994@gmail.com>
2020-07-06 22:28:48 -0700

    release v0.0.3

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import Amplify from 'aws-amplify';
 import { AppModule } from './app/app.module';
 import awsconfig from './aws-exports';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
 
 Amplify.configure(awsconfig);
 
